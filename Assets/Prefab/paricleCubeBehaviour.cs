184304cbb122f5afef7184585bba8877eb3e4dd0
Kayoung Lee <elain202@gmail.com>
2018-06-25 20:19:44 +0900

    RainSystem
    RainSystem final

---
+++ Assets/Prefab/paricleCubeBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paricleCubeBehaviour : MonoBehaviour {
+    public GameObject camera;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    Vector3 cubePosition;
+    void LateUpdate()
+    {
+        cubePosition.x = camera.transform.position.x;
+        cubePosition.y = camera.transform.position.y;
+        cubePosition.z = camera.transform.position.z;
+        transform.position = cubePosition;
+    }
+}
