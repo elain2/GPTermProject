142f8723153b716d4a3a97efcac0776c7046ffae
JeongSeokYoung <fjrdud@naver.com>
2018-06-26 01:05:31 +0900

    [Add] SoundSystem && Sound
    .

---
+++ Assets/Scripts/PlayerSound.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSound : MonoBehaviour {
+
+    public AudioClip JumpSound;
+    public AudioClip DeadSound;
+    public AudioClip WalkSound;
+
+    AudioSource myAudio;
+
+    public static PlayerSound instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        myAudio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void Jump()
+    {
+        myAudio.PlayOneShot(JumpSound);
+    }
+
+    public void Dead()
+    {
+        myAudio.PlayOneShot(DeadSound);
+    }
+
+    public void Walk()
+    {
+        myAudio.PlayOneShot(WalkSound);
+    }
+}
