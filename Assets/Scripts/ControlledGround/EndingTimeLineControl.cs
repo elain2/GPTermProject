4432cd146823e082ad8f9d5a83aff457c3ad91b2
DaeHyun Kang <limitedcopy@gmail.com>
2018-06-26 13:15:04 +0900

    마지막 후처리 커밋

---
+++ Assets/Scripts/ControlledGround/EndingTimeLineControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class EndingTimeLineControl : MonoBehaviour {
+
+    PlayableDirector director;
+    public bool b_play = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (b_play)
+        {
+            if(director.state != PlayState.Playing)
+            {
+                Application.Quit();
+            }
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("PlayerBody"))
+        {
+
+            other.GetComponent<PlayerPhysics>().b_CanControl = false;
+            director.Play();
+            b_play = true;
+        }
+    }
+
+
+}
